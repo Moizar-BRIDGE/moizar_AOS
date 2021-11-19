6b2e61b9acaeef0f80f63344007a144442db3c33
ksssssw <rtd0731@naver.com>
2021-11-20 01:31:12 +0900

    프로필 둘러보기 제작중

--- app/src/main/java/com/example/moizar/MainTeamsBtnView.kt
+++ app/src/main/java/com/example/moizar/MainTeamsBtnView.kt
@@ -2,26 +2,20 @@ package com.example.moizar
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class MainTeamsBtnView @JvmOverloads constructor(context: Context, attrs: AttributeSet?=null, defStyleAttr: Int=0)
-    : LinearLayout(context, attrs, defStyleAttr){
+class MainTeamsBtnView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    companion object{
+    companion object {
 //        private val dateFormat = SimpleDateFormat("yy/MM/dd HH:mm")
     }
 
-    init{
+    init {
         inflate(context, R.layout.teams_main_btn, this)
     }
-
-//    fun setAlarmDate(alarmDate: Date){
-//        if(alarmDate.before(Date())){   //현재 시간보다 이전이면 알람이 없다고 표시
-//            infoText.setText("알람이 없습니다.")
-//        }
-//        else{
-//            infoText.setText(dateFormat.format(alarmDate))
-//        }
-//    }
 
 }
