84b88ef42801b5fc69fd336d450291ba232892b9
ksssssw <rtd0731@naver.com>
2021-11-26 04:40:25 +0900

    프로필 보기 화면 완성

---
+++ app/src/main/java/com/example/moizar/RecyclerViewDecoration.kt
@@ -0,0 +1,20 @@
+package com.example.moizar
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+open class RecyclerViewDecoration(
+    private val divHeight : Int
+) : RecyclerView.ItemDecoration(){
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = divHeight
+        outRect.bottom = divHeight
+    }
+}
