d46347814c3d336b156b98f5132a9dd779d30bb9
조재원 <m970314@naver.com>
2021-11-26 04:43:53 +0900

    profile setting 구현

---
+++ app/src/main/java/com/example/moizar/Alarmdata.kt
@@ -0,0 +1,9 @@
+package com.example.moizar
+
+data class Alarmdata (
+    val name : String,
+    val name_after : String,
+    val Alarm_detail : String,
+    val notifi : Int,
+    val clear : Int
+)
---
+++ app/src/main/java/com/example/moizar/ChatListdata.kt
@@ -0,0 +1,9 @@
+package com.example.moizar
+
+data class ChatListdata (
+    val name : String,
+    val last_chat_num : String,
+    val last_chat : String,
+    val last_chat_time : String,
+    val user_image : Int
+)
