4e1a489535957242fb57c3726dc95de884b675a9
ksssssw <rtd0731@naver.com>
2021-11-26 00:44:44 +0900

    팀 작성하기 화면 완성

--- app/src/main/java/com/example/moizar/ViewMainFragment.kt
+++ app/src/main/java/com/example/moizar/ViewMainFragment.kt
@@ -1,5 +1,7 @@
 package com.example.moizar
 
+import android.content.ClipData
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,11 +9,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ScrollView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.NestedScrollView
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
 class ViewMainFragment : Fragment(), View.OnClickListener {
@@ -19,12 +25,26 @@ class ViewMainFragment : Fragment(), View.OnClickListener {
     private lateinit var listAdapter: ProfileRecyclerAdapter
     private lateinit var listAdapter2: CompetitionRecyclerAdapter
 
+    private val fragmentViewTeams by lazy { ViewTeamsFragment() }
+    private val fragmentViewPeoples by lazy { ViewPeoplesFragment() }
+    private val fragmentViewCompetition by lazy { ViewCompetitionFragment() }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view: View = inflater.inflate(R.layout.fragment_view_main, container, false)
+
+        // 탭 전환 버튼 부분
+        val swipeTeamsTabBtn: MainTeamsBtnView = view.findViewById(R.id.swipe_teams_tab_btn)
+        val swipeProfileTabBtn: TextView = view.findViewById(R.id.swipe_profile_tab_btn)
+        val swipeCompetitionTabBtn: TextView = view.findViewById(R.id.swipe_competitions_tab_btn)
+
+        swipeTeamsTabBtn.setOnClickListener(this)
+        swipeProfileTabBtn.setOnClickListener(this)
+        swipeCompetitionTabBtn.setOnClickListener(this)
+
         // Inflate the layout for this fragment
         return view
     }
@@ -57,19 +77,52 @@ class ViewMainFragment : Fragment(), View.OnClickListener {
         competition_recycler_view.itemAnimator = DefaultItemAnimator()
         competition_recycler_view.adapter = listAdapter2
 
-        val mainScrollView : NestedScrollView = view.findViewById(R.id.main_scrollView)
-        OverScrollDecoratorHelper.setUpOverScroll(profileList_recycler_view, OverScrollDecoratorHelper.ORIENTATION_HORIZONTAL)
+        val mainScrollView: NestedScrollView = view.findViewById(R.id.main_scrollView)
+        OverScrollDecoratorHelper.setUpOverScroll(
+            profileList_recycler_view,
+            OverScrollDecoratorHelper.ORIENTATION_HORIZONTAL
+        )
+
     }
 
 
     override fun onClick(v: View?) {
+        val teamsWriteBtn: FloatingActionButton =
+            requireActivity().findViewById(R.id.teams_write_btn)
+        val customBottomNavigationView1: CustomBottomNavigationView1 =
+            requireActivity().findViewById(R.id.customBottomBar)
+        val intent = Intent(activity, TeamsWriteActivity::class.java)
+
         when (v?.id) {
-            R.id.like_btn -> {
-                Log.d("btn", "눌림")
+            R.id.swipe_teams_tab_btn -> {
+                changeFragment(fragmentViewTeams)
+                teamsWriteBtn.visibility = View.VISIBLE
+                teamsWriteBtn.setOnClickListener {
+                    startActivity(intent)
+                }
+                customBottomNavigationView1.menu.findItem(R.id.action_main).isChecked = false
+                customBottomNavigationView1.menu.findItem(R.id.action_teams).isChecked = true
+            }
+            R.id.swipe_profile_tab_btn -> {
+                changeFragment(fragmentViewPeoples)
+                teamsWriteBtn.visibility = View.INVISIBLE
+                customBottomNavigationView1.menu.findItem(R.id.action_main).isChecked = false
+                customBottomNavigationView1.menu.findItem(R.id.action_peoples).isChecked = true
+            }
+            R.id.swipe_competitions_tab_btn -> {
+                changeFragment(fragmentViewCompetition)
+                teamsWriteBtn.visibility = View.INVISIBLE
+                customBottomNavigationView1.menu.findItem(R.id.action_main).isChecked = false
+                customBottomNavigationView1.menu.findItem(R.id.action_competition).isChecked = true
             }
             else -> {
             }
         }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        val fragManager: FragmentManager = requireActivity().getSupportFragmentManager()
+        fragManager.beginTransaction().replace(R.id.view_Pager, fragment).commit()
     }
 
 }
